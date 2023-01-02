39442a3aa06567e56aef521ca15bf9a3d1d63cd8
Khoa Nguyen <khoa@friendlydev.co>
2023-01-03 00:35:20 +0700

    remove nginx.conf, change port, add url

--- app/config/server.js
+++ app/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env.int('PUBLIC_URL', 'http://ec2-3-26-44-238.ap-southeast-2.compute.amazonaws.com/'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1bb4ad9372b82e2ae4636ec54e950ebc'),
