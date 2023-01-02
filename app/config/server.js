70651e8dbbbcfea7a6afc4400670ed91957551c6
Khoa Nguyen <khoa@friendlydev.co>
2023-01-03 01:28:29 +0700

    remove url

--- app/config/server.js
+++ app/config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://ec2-54-253-24-125.ap-southeast-2.compute.amazonaws.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1bb4ad9372b82e2ae4636ec54e950ebc'),
