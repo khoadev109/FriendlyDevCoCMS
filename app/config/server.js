7ce8bc674acbd549b875d3fb8537a2246d2eb7b8
Khoa Nguyen <khoa@friendlydev.co>
2023-01-02 13:23:15 +0700

    init project

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1bb4ad9372b82e2ae4636ec54e950ebc'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f7e4c973-7816-4aaf-81c0-448cebf4a2e8'
+};
