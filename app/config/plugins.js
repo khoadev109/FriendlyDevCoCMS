6dd2fafe8bf8e2fc3c9a21cc76ddd04fa3ce82d9
Khoa Nguyen <khoa@friendlydev.co>
2023-01-04 03:02:46 +0700

    update plugins

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -7,21 +7,24 @@ module.exports = ({ env }) => ({
     //   secret: env('WASABI_SECRET'),
     //   key: env('WASABI_KEY')
     // }
-    provider: "aws-s3",
-    providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: env("AWS_REGION"),
-      params: {
-        Bucket: env("AWS_BUCKET"),
-      },
-    },
     breakpoints: {
       xlarge: 1920,
       large: 1000,
       medium: 750,
       small: 254,
       xsmall: 64,
+    },
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID", "AKIAXDJ7JDMBH7TJI4QD"),
+      secretAccessKey: env(
+        "AWS_ACCESS_SECRET",
+        "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"
+      ),
+      region: env("AWS_REGION", "ap-southeast-2"),
+      params: {
+        Bucket: env("AWS_BUCKET", "strapifriendlydevco"),
+      },
     },
   },
 });
